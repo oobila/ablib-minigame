23afc759489a4db159d180053367973568d7da66
BuildTools <unconfigured@null.spigotmc.org>
2026-09-18 22:20:13 +0100

    updating cache type and gitignore

--- src/main/java/com/github/oobila/bukkit/minigame/Minigame.java
+++ src/main/java/com/github/oobila/bukkit/minigame/Minigame.java
@@ -3,12 +3,12 @@ package com.github.oobila.bukkit.minigame;
 import com.github.alastairbooth.abid.ABID;
 import com.github.oobila.bukkit.minigame.arena.Arena;
 import com.github.oobila.bukkit.minigame.environments.Environment;
-import com.github.oobila.bukkit.persistence.caches.standard.ReadAndWriteCache;
+import com.github.oobila.bukkit.persistence.caches.real.CombiCache;
 
 public interface Minigame {
 
-    ReadAndWriteCache<ABID, Environment> getEnvironmentCache();
+    CombiCache<ABID, Environment> getEnvironmentCache();
 
-    ReadAndWriteCache<ABID, Arena> getArenaCache();
+    CombiCache<ABID, Arena> getArenaCache();
 
 }
